e5efeb37ab12176fd9a115609a12eefeec33f9b5
Arnisha <arnishakapali11@gmail.com>
2024-10-25 23:20:41 +0600

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,4 +1,4 @@
 first_number = input("Enter the first number here : ")
 second_number = input ("Enter the second number herre : ")
 sum = int(first_number)+ float (second_number)
-print (sum)
+print ("sum"+ str(sum))
